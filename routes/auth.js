33ce2dad6bbf23b9253ba7283610d2fe7e19cb88
Jay Lim <limjay06@gmail.com>
2020-04-13 20:01:22 -0400

    Remove unnecessary comments and change 400 to 422 in errors res status method.

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post('/login', [
   const errors = validationResult(req)
 
   if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()})
+    return res.status(422).json({errors: errors.array()})
   }
 
   const {email, password} = req.body
--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.post('/signup', [
   const errors = validationResult(req)
 
   if(!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()})
+    return res.status(422).json({errors: errors.array()})
   }
 
   const {name, email, password, places} = req.body
